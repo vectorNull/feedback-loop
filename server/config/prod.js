e293a54dfb5005fc38f7b4ce9fb2de9381484d01
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-01-31 22:47:23 -0800

    Finished auth flow

---
+++ server/config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+
+    MONGO_DB_URI: process.env.MONGO_URI,
+
+    COOKIE_KEY: process.env.COOKIE_KEY,
+}
--- server/index.js
+++ server/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 require('./controllers/passport');
 
+mongoose.connect(keys.MONGO_DB_URI, () => {
+    console.log('Connected to database...')
+})
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        keys: [keys.COOKIE_KEY]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 require('./routes/authRoutes')(app);
 
